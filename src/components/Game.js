706ba97e2207d4f9f3798aeedd586b0a11335bf6
Samer Buna <samer.buna@gmail.com>
2016-12-15 10:08:12 -0800

    read from api

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,33 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 import Row from './Row';
 import Cell from './Cell';
 
+import config from '../config';
+
 class Game extends React.Component {
-  state = {test: 42};
+  state = { gridData: [] };
   challengeCells = { '0-0': true, '1-1' : true, '4-4': true };
 
   // fetch the data, http://server/api/index
+  componentDidMount() {
+    axios.get(`${config.serverUrl}/api/index`)
+      .then(resp => {
+        this.setState({gridData: resp.data.grid});
+      });
+  }
+
   render() {
-    // let rows = [];
-    // for(let r = 0; r<5; r++) {
-    //   let cells = [];
-    //   for(let c = 0; c<5; c++) {
-    //     const cellId = `${r}-${c}`;
-    //     const isActive = this.challengeCells[cellId];
-    //     cells.push(<Cell status={isActive ? 'active' : ''} id={cellId} key={cellId} />);
-    //   }
-    //   rows.push(
-    //     <Row className="row" key={r}>
-    //       {cells}
-    //     </Row>
-    //   );
-    // }
-    //
+    const grid = this.state.gridData.map((row, index) => {
+      return (
+        <Row key={index}>
+          {row.map(cellId => <Cell id={cellId} key={cellId} />)}
+        </Row>
+      );
+    });
+
     return (
       <div>
         {this.state.test}
-        {rows}
+        {grid}
       </div>
     );
   }
